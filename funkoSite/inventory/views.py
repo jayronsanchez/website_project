7be89ac2efc03834599bcbd77913c4efff3af344
Jayron Lester Sanchez <jayronsanchez@gmail.com>
2019-03-17 13:19:17 +0800

    Updated dibs functionality. Made some code and repository clean ups.

--- funkoSite/inventory/views.py
+++ funkoSite/inventory/views.py
@@ -1,66 +1,8 @@
-# from django.shortcuts import render
-# from django.urls import reverse_lazy
-# from django.views.generic import (View, TemplateView,
-#                                   ListView, DetailView,
-#                                   CreateView, UpdateView,
-#                                   DeleteView)
-# from django.http import HttpResponse
-# from . import models
-
-# # Create your views here.
-
-# # Simple class based view
-# # class CBView(View):
-# #     def get(self, request):
-# #         return HttpResponse("Class Based Views Are Cool!")
-
-# class IndexView(TemplateView):
-#     # will look into TEMPLATE_DIR in settings.py
-#     template_name = 'index.html'
-
-#     # keyword arguments. ** (double asterisk) will accept parameter as a dictionary
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['injectme'] = 'BASIC INJECTION'
-#         return context
-
-# class SchoolListView(ListView):
-#     model = models.School
-#     # returns school_list by default to be used for template tag
-#     # default naming of django: lower case the model name, then add _list...
-#     # if you want to rename school_list...
-#     context_object_name = 'schools'
-#     # i think you may assign a template_name also, but by default it would be school_list.html  
-
-# class SchoolDetailView(DetailView):
-#     # rename the default return 'school' to be used for template tag
-#     context_object_name = 'school_detail'
-#     model = models.School
-#     # this will look into templates folder of the application
-#     template_name = 'basic_app/school_detail.html'
-
-# class SchoolCreateView(CreateView):
-#     # default template name school_form.html
-#     fields = ('name', 'principal', 'location')
-#     model = models.School
-
-# class SchoolUpdateView(UpdateView):
-#     fields = ('name', 'principal')
-#     model = models.School
-
-# class SchoolDeleteView(DeleteView):
-#     model = models.School
-#     # if delete success, go back to the list
-#     success_url = reverse_lazy('basic_app:list')
-
-
-# from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import (ListView, DetailView,
                                   CreateView, UpdateView,
                                   DeleteView, TemplateView,
                                   RedirectView)
-# from django.http import HttpResponse
 from inventory import models
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
@@ -77,44 +19,13 @@ class CategoryUpdateView(UpdateView):
 
 class CategoryDeleteView(DeleteView):
     model = models.Category
-    # if delete success, go back to the list. Default template name is category_confirm_delete
     success_url = reverse_lazy('inventory:system_tailoring_category_list')
 
 class CategoryListView(ListView):
-     model = models.Category
-#     # returns school_list by default to be used for template tag
-#     # default naming of django: lower case the model name, then add _list...
-#     # if you want to rename school_list...
-#     context_object_name = 'schools'
-#     # i think you may assign a template_name also, but by default it would be school_list.html
+    model = models.Category
 
 class CategoryDetailView(DetailView):
-#     # rename the default return 'school' to be used for template tag
-#     context_object_name = 'school_detail'
-     model = models.Category
-#     # this will look into templates folder of the application
-#     template_name = 'basic_app/school_detail.html'
-
-
-# class CategoryView(View):
-#     form_class = forms.CategoryForm
-#     success_url = reverse_lazy('home')
-
-#     def get(self, request, *args, **kwargs):
-#         form = self.form_class()
-#         return render(request, 'inventory/category_form.html', {'form': form})
-
-#     def post(self, request, *args, **kwargs):
-#         form = self.form_class(request.POST, request.FILES)
-#         print("post")
-#         if form.is_valid():
-#             print("test")
-#             if 'category_image' in request.FILES:
-#                 models.Category.profile_pic = request.FILES['category_image']
-#             form.save()
-#             return redirect(self.success_url)
-#         else:
-#             return render(request, 'inventory/category_form.html', {'form': form})
+    model = models.Category
 
 class ItemCreateView(CreateView):
     # default template name:item_form
@@ -127,7 +38,7 @@ class ItemUpdateView(UpdateView):
 
 class ItemDeleteView(DeleteView):
     model = models.Item
-    # if delete success, go back to the list. Default template name is category_confirm_delete
+    # if delete success, go back to the list.
     success_url = reverse_lazy('inventory:system_tailoring_item_list')
 
 class ItemListView(ListView):
@@ -145,29 +56,29 @@ class ItemDibs(LoginRequiredMixin, RedirectView):
         try:
             models.UserDibs.objects.create(user=self.request.user, item=item)
         except IntegrityError:
-            messages.warning(self.request, 'Warning already a member!')
+            messages.warning(self.request, 'Warning! Item already added.')
         else:
-            messages.success(self.request, 'You are now a member!')
+            messages.success(self.request, 'Item Added!')
 
         return super().get(request, *args, **kwargs)
 
-# class ItemUndibs(LoginRequiredMixin, generic.RedirectView):
-#     def get_redirect_url(self, *args, **kwargs):
-#         return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+class ItemUndibs(LoginRequiredMixin, RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('inventory:detail_item', kwargs={'pk':self.kwargs.get('pk')})
 
-#     def get(self, request, *args, **kwargs):
-#         try:
-#             membership = models.GroupMember.objects.filter(
-#                 user=self.request.user,
-#                 group__slug=self.kwargs.get('slug')
-#             ).get()
-#         except models.GroupMember.DoesNotExist:
-#             messages.warning(self.request, 'Sorry you are not in this group!')
-#         else:
-#             membership.delete()
-#             messages.success(self.request, 'You have left the group!')
+    def get(self, request, *args, **kwargs):
+        try:
+            dibs = models.UserDibs.objects.filter(
+                user=self.request.user,
+                item=self.kwargs.get('pk')
+            ).get()
+        except models.UserDibs.DoesNotExist:
+            messages.warning(self.request, 'Item not in your list!')
+        else:
+            dibs.delete()
+            messages.success(self.request, 'Item removed!')
 
-#         return super().get(request, *args, **kwar
+        return super().get(request, *args, **kwargs)
 
 class SystemTailoringCategory(TemplateView):
     template_name = 'system_tailoring\system_tailoring_category.html'
