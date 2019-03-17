20428a640d515bbd018ca0283c21bde6e36c6692
Jayron Lester Sanchez <jayronsanchez@gmail.com>
2019-03-17 14:19:56 +0800

    updated settings.py ALLOWED_HOSTS to include pythonanywhere and localhost

--- funkoSite/funkoSite/settings.py
+++ funkoSite/funkoSite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'rl$c#&fu)xh+uf#+4+0h6(ig)czt0mg+=43a5cgh*q7yl@w4p)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jayronsanchez.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
