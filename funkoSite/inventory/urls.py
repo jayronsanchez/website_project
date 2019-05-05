68e4f2371042b5937f29e61c12eb89c6f3b57f77
Jayron Lester Sanchez <jayronsanchez@gmail.com>
2019-05-05 11:40:47 +0800

    added countdown js and functionality

--- funkoSite/inventory/urls.py
+++ funkoSite/inventory/urls.py
@@ -5,6 +5,7 @@ app_name = 'inventory'
 
 urlpatterns = [
     # for system_tailoring
+    #  apply RESTful architecture ea: /categories/<int>
     path('system_tailoring/category/', views.SystemTailoringCategory.as_view(), name='system_tailoring_category'),
     path('system_tailoring/category/create_category/', views.CategoryCreateView.as_view(), name='create_category'),
     path('system_tailoring/category/category_list/', views.CategoryListView.as_view(template_name='system_tailoring/system_tailoring_category_list.html'), name='system_tailoring_category_list'),
@@ -16,15 +17,18 @@ urlpatterns = [
     path('system_tailoring/item/<int:pk>/', views.ItemUpdateView.as_view(), name='update_item'),
     path('system_tailoring/item/remove/<int:pk>/', views.ItemDeleteView.as_view(), name='delete_item'),
     #for category
-    path('list_category/', views.CategoryListView.as_view(), name='list_category'),
-    path('list_category/<int:pk>/', views.CategoryDetailView.as_view(), name='detail_category'),
+    #  apply RESTful architecture ea: /categories/<int>
+    path('categories/', views.CategoryListView.as_view(), name='list_category'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='detail_category'),
     # for items
-    path('list_item/', views.ItemListView.as_view(), name='list_item'),    
-    path('list_item/<int:pk>', views.ItemDetailView.as_view(), name='detail_item'),    
-    path('list_item/dibs/<int:pk>', views.ItemDibs.as_view(), name='item_dibs'),    
-    path('list_item/undibs/<int:pk>', views.ItemUndibs.as_view(), name='item_undibs'),
+    #  apply RESTful architecture ea: /items/<int>
+    path('items/', views.ItemListView.as_view(), name='list_item'),    
+    path('items/<int:pk>', views.ItemDetailView.as_view(), name='detail_item'),    
+    path('items/dibs/<int:pk>', views.ItemDibs.as_view(), name='item_dibs'),    
+    path('items/undibs/<int:pk>', views.ItemUndibs.as_view(), name='item_undibs'),
     # for user dibs
-    path('my_items/<username>', views.DibsListView.as_view(), name='item_dibs_list'),     
-    path('list_item/<int:pk>/form', views.update_dibs, name='item_dibs_update_redirect'),
-    path('my_items/<username>/item/<int:pk>', views.DibsUpdateView.as_view(), name='item_dibs_update'),
+    #  apply RESTful architecture ea: /<username>/items/<item:id>
+    path('<username>/my_items', views.DibsListView.as_view(), name='item_dibs_list'),     
+    path('items/<int:pk>/form', views.update_dibs, name='item_dibs_update_redirect'),
+    path('<username>/my_items/item/<int:pk>', views.DibsUpdateView.as_view(), name='item_dibs_update'),
 ]
